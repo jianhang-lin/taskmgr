1661efc8be2d2458c049966c60c5624ac4231fd7
JianhangLin <linjianhang1010@gmail.com>
2020-03-18 11:24:57 +0800

    feat(sidebar): add sidebar component to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,12 @@ import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   imports: [
     CommonModule
   ]
