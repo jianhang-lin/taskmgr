e10a4793672c795bb0a0f0f7f168995d2b3d4f8e
JianhangLin <linjianhang1010@gmail.com>
2020-03-20 14:17:18 +0800

    feat(core): import BrowserAnimationsModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,8 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -19,8 +19,8 @@ import { loadSvgResouces } from '../utils/svg.util';
     SidebarComponent
   ],
   imports: [
-    CommonModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     SharedModule
   ]
 })
