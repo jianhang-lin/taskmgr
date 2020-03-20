3b8718f2489319fdb5b53df14898c60e20c1e413
JianhangLin <linjianhang1010@gmail.com>
2020-03-20 14:18:23 +0800

    feat(login): import LoginRoutingModule

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '../shared/shared.module';
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ LoginComponent ],
   imports: [
-    CommonModule
+    SharedModule,
+    LoginRoutingModule
   ]
 })
 export class LoginModule { }
