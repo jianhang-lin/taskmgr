c42172f9bc99e51352dd7c36b54a3b3cde8fc703
JianhangLin <linjianhang1010@gmail.com>
2020-03-20 14:28:12 +0800

    feat(app): import LoginModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
-    path: 'path',
-    component: AppComponent
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login/login.component';
+import { LoginModule } from './login/login.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     CoreModule,
     MatSidenavModule,
-    BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
