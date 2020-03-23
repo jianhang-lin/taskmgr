52b51281e9da94490e078104439f40960429fb51
JianhangLin <linjianhang1010@gmail.com>
2020-03-23 13:44:03 +0800

    feat(project): add new project component

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectItemComponent } from './project-item/project-item.component';
+import { NewProjectComponent } from './new-project/new-project.component';
 
 
 
 @NgModule({
-  declarations: [ProjectListComponent, ProjectItemComponent],
+  declarations: [ProjectListComponent, ProjectItemComponent, NewProjectComponent],
   imports: [
     CommonModule
   ]
