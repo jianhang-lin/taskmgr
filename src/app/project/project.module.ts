be9381598ddf127ddc6d0d8495a05fbd78a1ecc0
JianhangLin <linjianhang1010@gmail.com>
2020-03-23 13:44:43 +0800

    feat(project): add invite component

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectItemComponent } from './project-item/project-item.component';
 import { NewProjectComponent } from './new-project/new-project.component';
+import { InviteComponent } from './invite/invite.component';
 
 
 
 @NgModule({
-  declarations: [ProjectListComponent, ProjectItemComponent, NewProjectComponent],
+  declarations: [ProjectListComponent, ProjectItemComponent, NewProjectComponent, InviteComponent],
   imports: [
     CommonModule
   ]
