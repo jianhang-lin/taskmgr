20861b9bf06c3e20eb2fa389f75160f1a8f28c5b
JianhangLin <linjianhang1010@gmail.com>
2020-03-23 13:41:44 +0800

    feat(project): add project list component

---
+++ src/app/project/project.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectListComponent } from './project-list/project-list.component';
+
+
+
+@NgModule({
+  declarations: [ProjectListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProjectModule { }
