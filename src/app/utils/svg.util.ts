def7dcfa8409b7f3fa8730768357a5b2723fd2a9
JianhangLin <linjianhang1010@gmail.com>
2020-03-23 10:49:47 +0800

    feat(sidebar): add list to the sidebar

--- src/app/utils/svg.util.ts
+++ src/app/utils/svg.util.ts
@@ -2,6 +2,12 @@ import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
 export const loadSvgResouces = (ir: MatIconRegistry, ds: DomSanitizer) => {
-  ir.addSvgIcon('list', ds.bypassSecurityTrustResourceUrl('assets/list-24px.svg'));
+  const imgDir = 'assets/img';
+  const sidebarDir = `${imgDir}/sidebar`;
+  ir.addSvgIcon('day', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/day.svg`));
+  ir.addSvgIcon('month', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/month.svg`));
+  ir.addSvgIcon('project', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/project.svg`));
+  ir.addSvgIcon('projects', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/projects.svg`));
+  ir.addSvgIcon('week', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/week.svg`));
 };
 
