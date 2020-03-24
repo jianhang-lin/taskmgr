67fe5f64a9431b78dfa90cb5fe6a54bfaf36d65f
JianhangLin <linjianhang1010@gmail.com>
2020-03-24 09:56:01 +0800

    feat(copy-task): add copy task component

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -6,10 +6,11 @@ import { TaskListComponent } from './task-list/task-list.component';
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TaskHeaderComponent } from './task-header/task-header.component';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { CopyTaskComponent } from './copy-task/copy-task.component';
 
 
 @NgModule({
-  declarations: [TaskHomeComponent, TaskListComponent, TaskItemComponent, TaskHeaderComponent, NewTaskComponent],
+  declarations: [TaskHomeComponent, TaskListComponent, TaskItemComponent, TaskHeaderComponent, NewTaskComponent, CopyTaskComponent],
   imports: [
     SharedModule,
     TaskRoutingModule
