fa30e6a5689f04cc3e47de8fb77f775d5de95110
JianhangLin <linjianhang1010@gmail.com>
2020-03-25 23:27:30 +0800

    feat(animations): update translateX px to %

--- src/app/anim/router.anim.ts
+++ src/app/anim/router.anim.ts
@@ -4,11 +4,11 @@ export const slideToRight = trigger('routeAnim', [
   state('void', style({position: 'fixed', width: '100%', height: '80%'})),
   state('*', style({position: 'fixed', width: '100%', height: '80%'})),
   transition('void => *', [
-    style({transform: 'translateX(-100px)'}),
+    style({transform: 'translateX(-100%)'}),
     animate('0.5s ease-in-out', style({transform: 'translateX(0)'}))
   ]),
   transition('* => void', [
     style({transform: 'translateX(0)'}),
-    animate('0.5s ease-in-out', style({transform: 'translateX(100px)'}))
+    animate('0.5s ease-in-out', style({transform: 'translateX(100%)'}))
   ]),
 ]);
