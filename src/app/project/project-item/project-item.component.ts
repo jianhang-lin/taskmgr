9bc2a4232f7069be7646d43374b49df24cfa74a7
JianhangLin <linjianhang1010@gmail.com>
2020-03-25 16:22:36 +0800

    feat(project-item): add card animations

--- src/app/project/project-item/project-item.component.ts
+++ src/app/project/project-item/project-item.component.ts
@@ -1,24 +1,38 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, HostBinding, HostListener } from '@angular/core';
+import { cardAnim } from '../../anim/card.anim';
 
 @Component({
   selector: 'app-project-item',
   templateUrl: './project-item.component.html',
-  styleUrls: ['./project-item.component.scss']
+  styleUrls: ['./project-item.component.scss'],
+  animations: [
+    cardAnim
+  ]
 })
 export class ProjectItemComponent implements OnInit {
 
   @Input() item;
-
   // tslint:disable-next-line:no-output-on-prefix
   @Output() onInvite = new EventEmitter<void>();
   // tslint:disable-next-line:no-output-on-prefix
   @Output() onEdit = new EventEmitter<void>();
   // tslint:disable-next-line:no-output-on-prefix
   @Output() onDel = new EventEmitter<void>();
+  @HostBinding('@card') cardState = 'out';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.cardState = 'hover';
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.cardState = 'out';
   }
 
   onInviteClick() {
