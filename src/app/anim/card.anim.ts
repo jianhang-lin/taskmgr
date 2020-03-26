b3dca574a0b32c098fad1d78b52016a3b5916230
JianhangLin <linjianhang1010@gmail.com>
2020-03-26 09:02:05 +0800

    fix(animations): replace border-radius property with box-shadow property

--- src/app/anim/card.anim.ts
+++ src/app/anim/card.anim.ts
@@ -1,8 +1,8 @@
 import { trigger, state, transition, style, animate, keyframes } from '@angular/animations';
 
 export const cardAnim = trigger('card', [
-  state('out', style({transform: 'scale(1)', 'border-radius': '0px'})),
-  state('hover', style({transform: 'scale(1.1)', 'border-radius': '40px'})),
-  transition('out => hover', animate('100ms ease-in')),
-  transition('hover => out', animate('100ms ease-out')),
+  state('out', style({transform: 'scale(1)', 'box-shadow': 'none'})),
+  state('hover', style({transform: 'scale(1.1)', 'box-shadow': '3px 3px 5px 6px #ccc'})),
+  transition('out => hover', animate('200ms ease-in')),
+  transition('hover => out', animate('200ms ease-out')),
 ]);
