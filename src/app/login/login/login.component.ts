b1c7ee045f5c0765a4429270e47a78ee089812c5
JianhangLin <linjianhang1010@gmail.com>
2020-03-26 16:27:38 +0800

    feat(login): define form as FormGroup and init it

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  form: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('wang@163.com', Validators.compose([Validators.required, Validators.email])),
+      password: new FormControl()
+    });
   }
 
+  onSubmit(form: FormGroup, ev: Event) {
+  }
 }
