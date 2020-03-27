0b464c77772ba2176125d6c2c9ab5012e5884917
JianhangLin <linjianhang1010@gmail.com>
2020-03-27 12:44:18 +0800

    feat(services): add quote service

---
+++ src/app/services/quote.service.ts
@@ -0,0 +1,17 @@
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Quote } from '@angular/compiler';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuoteService {
+
+  constructor(private http: HttpClient, @Inject('BASE_CONFIG') private config) { }
+
+  getQuote(): Observable<any> {
+    const uri = `${this.config.uri}/quotes/${Math.floor(Math.random() * 10)}`;
+    return this.http.get<Observable<Quote>>(uri);
+  }
+}
