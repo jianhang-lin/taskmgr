3498afa2b42ab6b64b1b9441b83d6d31f5be2895
JianhangLin <linjianhang1010@gmail.com>
2020-04-02 08:38:47 +0800

    feat(core): import AppStoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from '../app-routing.module';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppStoreModule } from '../reducers';
 import { SharedModule } from '../shared/shared.module';
 import { ServicesModule } from '../services/services.module';
 import { HeaderComponent } from './header/header.component';
@@ -25,7 +26,8 @@ import { debug } from '../utils/debug.util';
     BrowserAnimationsModule,
     SharedModule,
     AppRoutingModule,
-    ServicesModule.forRoot()
+    ServicesModule.forRoot(),
+    AppStoreModule
   ],
   providers: [
     {
