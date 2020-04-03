adae847574175711c165095631544a94bd324317
JianhangLin <linjianhang1010@gmail.com>
2020-04-03 08:58:39 +0800

    fix(service): check toekn in AuthGuardService

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,15 +1,30 @@
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import * as fromRoot from '../reducers';
+import { Store } from '@ngrx/store';
+import { getAuthState } from '../reducers';
+import { defaultIfEmpty, map } from 'rxjs/operators';
+import * as authActions from '../actions/auth.action';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
+
+  constructor(private store$: Store<fromRoot.State>) {
+
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return of(true);
+    return this.store$.select(getAuthState).pipe(map(auth => {
+      const result = auth.token !== null && auth.token !== undefined;
+      if (result) {
+        this.store$.dispatch(new authActions.LoginAction({email: '', password: ''}));
+      }
+      return result;
+    }), defaultIfEmpty(false));
   }
 
 }
