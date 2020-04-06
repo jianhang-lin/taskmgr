536517f0e820b3118b2cf5268db07b4af29b4307
JianhangLin <linjianhang1010@gmail.com>
2020-04-06 16:16:25 +0800

    perf(task): improve new task component

--- src/app/task/new-task/new-task.component.ts
+++ src/app/task/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
@@ -24,15 +25,35 @@ export class NewTaskComponent implements OnInit {
       value: 3
     }
   ];
-
-  constructor(@Inject(MAT_DIALOG_DATA) private data) { }
+  form: FormGroup;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data,
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<NewTaskComponent>
+  ) { }
 
   ngOnInit(): void {
     this.title = this.data.title;
-    console.log(JSON.stringify(this.data.task));
+    this.form = this.fb.group({
+      desc: [this.data.task ? this.data.task.desc : '', Validators.required],
+      priority: [this.data.task ? this.data.task.priority : 3, Validators.required],
+      owner: [this.data.task ? [this.data.task.owner] : [this.data.owner]],
+      followers: [this.data.task ? [...this.data.task.participants] : []],
+      dueDate: [this.data.task ? this.data.task.dueDate : ''],
+      reminder: [this.data.task ? this.data.task.reminder : ''],
+      remark: [this.data.task ? this.data.task.remark : '']
+    });
   }
 
-  onClick() {
-
+  onSubmit(ev: Event, {value, valid}) {
+    ev.preventDefault();
+    if (!valid) {
+      return;
+    }
+    this.dialogRef.close({
+      ...value,
+      ownerId: value.owner.length > 0 ? value.owner[0].id : null,
+      participantIds: value.followers.map(f => f.id),
+    });
   }
 }
