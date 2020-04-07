ae9f201216825474b8976db49f41ddcf073694d1
JianhangLin <linjianhang1010@gmail.com>
2020-04-07 13:58:55 +0800

    feat(calendar): implement calendar home component

--- src/app/my-calendar/calendar-home/calendar-home.component.ts
+++ src/app/my-calendar/calendar-home/calendar-home.component.ts
@@ -1,4 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { CalendarEventTimesChangedEvent } from 'angular-calendar';
+import {map, switchMap} from 'rxjs/operators';
+import { TaskService } from '../../services/task.service';
+import { Store } from '@ngrx/store';
+import * as fromRoot from '../../reducers';
+import { startOfDay, endOfDay } from 'date-fns';
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#d1e8ff'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#fdf1ba'
+  }
+};
+
+const getColor = (priority: number) => {
+  switch (priority) {
+    case 1:
+      return colors.red;
+    case 2:
+      return colors.blue;
+    default:
+      return colors.yellow;
+  }
+};
 
 @Component({
   selector: 'app-calendar-home',
@@ -7,9 +41,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarHomeComponent implements OnInit {
 
-  constructor() { }
+  viewDate: Date;
+  view$: Observable<string>;
+  events$: Observable<CalendarEventTimesChangedEvent>;
+  constructor(private route: ActivatedRoute,
+              private service$: TaskService,
+              private store$: Store<fromRoot.State>) {
+    this.viewDate = new Date();
+    this.view$ = this.route.paramMap.pipe(map(p => p.get('view')));
+    // @ts-ignore
+    this.events$ = this.store$.select(fromRoot.getAuthState).pipe(
+      map(auth => auth.user.id),
+      switchMap(userId => this.service$.getUserTasks(userId)),
+      map(tasks => tasks.map(task => {
+        return {
+          start: startOfDay(task.createDate),
+          end: endOfDay(task.dueDate),
+          title: task.desc,
+          color: getColor(task.priority)
+        };
+      }))
+    );
+  }
 
   ngOnInit(): void {
   }
 
+  handleEvent(event: any) {
+
+  }
 }
--- src/app/my-calendar/my-calendar.module.ts
+++ src/app/my-calendar/my-calendar.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarHomeComponent } from './calendar-home/calendar-home.component';
-
+import { CalendarModule } from 'angular-calendar';
+import { MyCalendarRoutingModule } from './my-calendar-routing.module';
 
 
 @NgModule({
   declarations: [CalendarHomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MyCalendarRoutingModule,
+    CalendarModule
   ]
 })
 export class MyCalendarModule { }
