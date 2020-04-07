5147d5d8b466cebad4a6d255497c8872fff6463c
JianhangLin <linjianhang1010@gmail.com>
2020-04-07 12:30:12 +0800

    feat(my-calendar): add calendar home component

--- src/app/my-calendar/my-calendar.module.ts
+++ src/app/my-calendar/my-calendar.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalendarHomeComponent } from './calendar-home/calendar-home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CalendarHomeComponent],
   imports: [
     CommonModule
   ]
